83f3a4cfb86f250855f23bae1ec3bd2edc30b57a
Denny Portillo <d3portillo@gmail.com>
2018-06-28 21:57:02 -0600

    Update student_marksheet.php

--- application/views/backend/student/student_marksheet.php
+++ application/views/backend/student/student_marksheet.php
@@ -1,26 +1,212 @@
-<style type="text/css">
-<!--
-.style2 {color: #990000}
-.style3 {font-size: 18px}
--->
+<style>
+    .exam_chart {
+    width           : 100%;
+        height      : 265px;
+        font-size   : 11px;
+}
+  
 </style>
-<p align="center" class="style3"><strong>Disabled in Basic Version</strong></p>
-<p align="center" class="style3"><strong><a href="http://phpsoftwares.com/" target="_blank" class="style2"><u>Upgrade</u></a> to full version for just 14.99$ only. <u>100% Source Code</u> <a href="http://phpsoftwares.com/" target="_blank"><u><span class="style2">www.PhpSoftwares.com</span> </u></a></strong></p>
-<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-<div align="center">
-  <input type="hidden" name="cmd" value="_s-xclick">
-  <input type="hidden" name="hosted_button_id" value="4BKA5ZHWXARDU">
+<link rel="stylesheet" href="bulma.min.css">
+<script src="dist/html2pdf.bundle.min.js"></script>
+<script>
+function toPDF(id){
+    let element = document.createElement("div")
+
+  $.post("index.php?notas/getReview",{student_id: id},
+      function( data ) {
+      console.log(data)
+     element.innerHTML  = data
+  }).done(()=>{
+    var opt = {
+    margin:       1,
+    filename:     'notas.pdf',
+    image:        { type: 'jpeg', quality: 1},
+    html2canvas:  { scale: 2 },
+    jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+    };
+
+    // New Promise-based usage:
+    html2pdf().from(element).set(opt).save();
+  })
+
+
+
+
+
+}
+</script>
+<div class="pdfer">
+<?php 
+    $student_info = $this->crud_model->get_student_info($student_id);
+    $exams         = $this->crud_model->get_exams();
+    foreach ($student_info as $row1):
+    foreach ($exams as $row2):
+?>
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="panel panel-primary panel-shadow" data-collapsed="0">
+            <div class="panel-heading">
+                <div class="panel-title"><?php echo $row2['name'];?></div>
+            </div>
+            <div class="panel-body">
+                <style>
+                table tr td:nth-last-child(2){
+                    display: none !important
+                }
+
+               
+                </style>
+                
+               <div class="col-md-6">
+                   <table class="table table-bordered">
+                       <thead>
+                        <tr>
+                            <td style="text-align: center;">Materia</td>
+                            <td style="text-align: center;">Nota</td>
+                            <td style="text-align: center;">Nota más alta</td>
+                            <td style="text-align: center;"></td>
+                            <td style="text-align: center;">Comentarios</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php 
+                            $total_marks = 0;
+                            $total_grade_point = 0;
+                            $subjects = $this->db->get_where('subject' , array('class_id' => $class_id))->result_array();
+                            foreach ($subjects as $row3):
+                        ?>
+                            <tr>
+                                <td style="text-align: center;"><?php echo $row3['name'];?></td>
+                                <td style="text-align: center;">
+                                    <?php
+                                        $obtained_mark_query = $this->db->get_where('mark' , array(
+                                                    'subject_id' => $row3['subject_id'],
+                                                        'exam_id' => $row2['exam_id'],
+                                                            'class_id' => $class_id,
+                                                                'student_id' => $student_id));
+                                        if ( $obtained_mark_query->num_rows() > 0) {
+                                            $marks = $obtained_mark_query->result_array();
+                                            foreach ($marks as $row4) {
+                                                echo $row4['mark_obtained'];
+                                                $total_marks += $row4['mark_obtained'];
+                                            }
+                                        }
+                                    ?>
+                                </td>
+                                <td style="text-align: center;">
+                                    <?php
+
+                                    $highest_mark = $this->crud_model->get_highest_marks( $row2['exam_id'] , $class_id , $row3['subject_id'] );
+                                    echo $highest_mark;
+
+
+        
+                                    ?>
+                                </td>
+                                <td style="text-align: center;">
+                                    <?php
+                                        if ($row4['mark_obtained'] >= 0 || $row4['mark_obtained'] != '') {
+                                            $grade = $this->crud_model->get_grade($row4['mark_obtained']);
+                                            echo $grade['name'];
+                                            $total_grade_point += $grade['grade_point'];
+                                        }
+                                    ?>
+                                </td>
+                                <td style="text-align: center;"><?php echo $row4['comment'];?></td>
+                            </tr>
+                        <?php endforeach;?>
+                    </tbody>
+                   </table>
+
+                   
+
+                   
+                   
+               </div>
+
+               <div class="col-md-6">
+                   <div id="chartdiv<?php echo $row2['exam_id'];?>" class="exam_chart"></div>
+                       <script type="text/javascript">
+                            var chart<?php echo $row2['exam_id'];?> = AmCharts.makeChart("chartdiv<?php echo $row2['exam_id'];?>", {
+                                "theme": "none",
+                                "type": "serial",
+                                "dataProvider": [
+                                        <?php 
+                                            foreach ($subjects as $subject) :
+                                        ?>
+                                        {
+                                            "subject": "<?php echo $subject['name'];?>",
+                                            "mark_obtained": 
+                                            <?php
+                                                $obtained_mark = $this->crud_model->get_obtained_marks( $row2['exam_id'] , $class_id , $subject['subject_id'] , $row1['student_id']);
+                                                echo $obtained_mark;
+                                            ?>,
+                                            "mark_highest": 
+                                            <?php
+                                                $highest_mark = $this->crud_model->get_highest_marks( $row2['exam_id'] , $class_id , $subject['subject_id'] );
+                                                echo $highest_mark;
+                                            ?>
+                                        },
+                                        <?php 
+                                            endforeach;
+
+                                        ?>
+                                    
+                                ],
+                                "valueAxes": [{
+                                    "stackType": "3d",
+                                    "unit": "%",
+                                    "position": "left",
+                                    "title": "Eficiencia"
+                                }],
+                                "startDuration": 1,
+                                "graphs": [{
+                                    "balloonText": "Nota obtenida [[category]]: <b>[[value]]</b>",
+                                    "fillAlphas": 0.9,
+                                    "lineAlpha": 0.2,
+                                    "title": "2004",
+                                    "type": "column",
+                                    "fillColors":"#7f8c8d",
+                                    "valueField": "mark_obtained"
+                                }, {
+                                    "balloonText": "Nota más alta [[category]]: <b>[[value]]</b>",
+                                    "fillAlphas": 0.9,
+                                    "lineAlpha": 0.2,
+                                    "title": "2005",
+                                    "type": "column",
+                                    "fillColors":"#34495e",
+                                    "valueField": "mark_highest"
+                                }],
+                                "plotAreaFillAlphas": 0.1,
+                                "depth3D": 20,
+                                "angle": 45,
+                                "categoryField": "subject",
+                                "categoryAxis": {
+                                    "gridPosition": "start"
+                                },
+                                "exportConfig":{
+                                    "menuTop":"20px",
+                                    "menuRight":"20px",
+                                    "menuItems": [{
+                                        "format": 'png'   
+                                    }]  
+                                }
+                            });
+                    </script>
+               </div>
+               
+            </div>
+        </div>  
+    </div>
 </div>
-<table>
-<tr><td><input type="hidden" name="on0" value="Select Software:">Upgrade Software:</td></tr><tr><td><select name="os0">
-	<option value="School Software">School Software $14.99 USD</option>
-	<option value="Hospital Software">Hospital Software $9.99 USD</option>
-	<option value="POS Billing Software">POS Billing Software $14.99 USD</option>
-	<option value="All 3 Softwares">All 3 Softwares $29.99 USD</option>
-</select> </td></tr>
-</table>
-<input type="hidden" name="currency_code" value="USD">
-<input type="image" src="https://www.paypalobjects.com/en_GB/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal � The safer, easier way to pay online.">
-<img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
-</form>
-<img src="uploads/images/student/exammarks_student.PNG" width="1039" height="721" />
+<?php
+    endforeach;
+        endforeach;
+?>
+                    </div>
+                    <script>
+window.onload = ()=>{
+    document.querySelector("h3").innerHTML = document.querySelector("h3").innerHTML.replace("Marksheet For","Notas de ")
+}
+</script>
