12726c7d91b5c9904e0799a9876b5d8f8a72ff87
Denny Portillo <d3portillo@gmail.com>
2018-06-28 22:03:46 -0600

    Update header.php

--- application/views/backend/header.php
+++ application/views/backend/header.php
@@ -81,7 +81,7 @@
 			
 			<li>
 				<a href="<?php echo base_url();?>index.php?login/logout">
-					Log Out <i class="entypo-logout right"></i>
+					Cerrar Session<i class="entypo-logout right"></i>
 				</a>
 			</li>
 		</ul>
